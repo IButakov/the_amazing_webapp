3f118b309f34b2cd09a33f606159aabf6e5607f4
Ivan Butakov <me@ibutakov.ru>
2022-06-12 17:11:29 +0700

    web alpha

---
+++ create.php
@@ -0,0 +1,19 @@
+<?php include "templates/header.php"; ?><h2>Add a user</h2>
+
+    <form method="post">
+    	<label for="firstname">Имя</label>
+    	<input type="text" name="firstname" id="firstname">
+    	<label for="lastname">Фамилия</label>
+    	<input type="text" name="lastname" id="lastname">
+    	<label for="email">Email</label>
+    	<input type="text" name="email" id="email">
+    	<label for="age">Возраст</label>
+    	<input type="text" name="age" id="age">
+    	<label for="location">Город</label>
+    	<input type="text" name="location" id="location">
+    	<input type="submit" name="submit" value="Submit">
+    </form>
+
+    <a href="index.php">Back to home</a>
+
+    <?php include "templates/footer.php"; ?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>The Amazing Web App!</title>
+
+    <link rel="stylesheet" href="css/style.css" />
+  </head>
+
+  <body>
+    <h1>The Amazing Web App!</h1>
+
+    <ul>
+      <li>
+        <a href="create.php"><strong>Добавить</strong></a> - Добавить данные в MySQL
+      </li>
+      <li>
+        <a href="read.php"><strong>Поиск</strong></a> - Найти запись
+      </li>
+    </ul>
+  </body>
+</html>
